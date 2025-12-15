32bbb2ca22c54fda1b8767f89968955c07d535c4
Mike Monaco <mamonaco1973@gmail.com>
2025-12-15 09:32:36 -0500

    v1 of packer build

--- validate.sh
+++ validate.sh
@@ -1,15 +1,16 @@
 
 #!/bin/bash
 
-NFS_IP=$(gcloud compute instances list \
-  --filter="name~'^nfs-gateway'" \
-  --format="value(networkInterfaces.accessConfigs[0].natIP)")
-
-echo "NOTE: Linux nfs-gateway public IP address is $NFS_IP"
-
-
 WIN_IP=$(gcloud compute instances list \
   --filter="name~'^win-ad'" \
   --format="value(networkInterfaces.accessConfigs[0].natIP)")
 
-echo "NOTE: Windows instance public IP address is $WIN_IP"
+echo "NOTE: Windows AD instance public IP address is $WIN_IP"
+
+
+
+NFS_IP=$(gcloud compute instances list \
+  --filter="name~'^xubuntu'" \
+  --format="value(networkInterfaces.accessConfigs[0].natIP)")
+
+echo "NOTE: Xubuntu public IP address is $NFS_IP"
